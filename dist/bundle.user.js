79bd3d4414b45cd0e6e1a926d945bb006d49bd5b
Namkit <leejayliang1@gmail.com>
2024-12-30 08:40:23 +0800

    Update bundle.user.js

--- dist/bundle.user.js
+++ dist/bundle.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         workflowy forwarder Plus
 // @namespace    http://tampermonkey.net/
-// @version      1.0.4
+// @version      1.0.7
 // @description  workflowy forwarder Plus
 // @author       Namkit
 // @match        https://workflowy.com/*
